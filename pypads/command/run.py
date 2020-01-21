5bd5d9dffcd9fcebf0345316feadb6f5319b3428
Thomas Weißgerber <weissger@posteo.de>
2020-01-21 16:06:18 +0100

    initial sklearn tracking

--- pypads/command/run.py
+++
@@ -1,9 +0,0 @@
-def run():
-    """
-    Function to run an arbitrary command in the git repository.
-    :return:
-    """
-    import subprocess
-    process = subprocess.Popen(['ls', '-l'], stdout=subprocess.PIPE)
-    for line in process.stdout:
-        print(line.decode().strip())
