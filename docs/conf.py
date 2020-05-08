bf4ec79c07e61f8873641f79fb97f68237e085f3
Mehdi Ben Amor <mehdi.benamor@uni-passau.de>
2020-05-08 13:32:31 +0200

    fixed path of pypads in sphinx conf file

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('sphinxext'))
-sys.path.insert(0, os.path.abspath('../../pypads'))
+sys.path.insert(0, os.path.abspath('../pypads'))
 from github_link import make_linkcode_resolve
 
 
