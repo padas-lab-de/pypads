8012019b207e39c9657ff77917499d40e60c8dd4
Thomas Weißgerber <weissger@posteo.de>
2020-05-19 20:13:54 +0200

    Fix: doc: Updated the doc to include references to other projects.

--- docs/conf.py
+++ docs/conf.py
@@ -10,12 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import os
+import sys
 
-# sys.path.insert(0, os.path.abspath('sphinxext'))
-# sys.path.insert(0, os.path.abspath('../..'))
-# sys.path.insert(0, os.path.abspath('..'))
-# from github_link import make_linkcode_resolve
-
+sys.path.insert(0, os.path.abspath('sphinxext'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
+from github_link import make_linkcode_resolve
 
 # -- Project information -----------------------------------------------------
 
