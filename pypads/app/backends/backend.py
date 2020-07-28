38e365856994eb278fe0469a7fccf559c4aa175a
Thomas Weißgerber <weissger@posteo.de>
2020-07-28 16:11:25 +0200

    WIP: dev: WIP for tracking objects (Bugfixes)

--- pypads/app/backends/backend.py
+++ pypads/app/backends/backend.py
@@ -155,7 +155,7 @@ class MLFlowBackend(BackendInterface):
 
     @property
     def mlf(self) -> MlflowClient:
-        return MlflowClient(self.uri).list_experiments()
+        return MlflowClient(self.uri)
 
     def store_tracked_object(self, to: TrackedObject, path=""):
         path += "{}#{}".format(to.__class__.__name__, id(to))
